dfb982fefc1fa0dbf107278006dbd015dd3e6ad5
Peerapong Maitriwong <peerapong@code-app.com>
2018-07-04 14:26:36 +0700

    updated browser detection

--- src/lib/browser-detection.js
+++ src/lib/browser-detection.js
@@ -2,97 +2,53 @@
 
 var MINIMUM_SUPPORTED_CHROME_IOS_VERSION = 48
 
-function isOperaMini(ua) {
-  ua = ua || global.navigator.userAgent
-  return ua.indexOf('Opera Mini') > -1
+function isOperaMini() {
+  return false;
 }
 
-function isAndroidFirefox(ua) {
-  ua = ua || global.navigator.userAgent
-  return isAndroid(ua) && ua.indexOf('Firefox') > -1
+function isAndroidFirefox() {
+  return false;
 }
 
-function getIEVersion(ua) {
-  ua = ua || global.navigator.userAgent
-
-  if (ua.indexOf('MSIE') !== -1) {
-    return parseInt(ua.replace(/.*MSIE ([0-9]+)\..*/, '$1'), 10)
-  } else if (/Trident.*rv:11/.test(ua)) {
-    return 11
-  }
-
-  return null
+function getIEVersion() {
+  return false;
 }
 
-function isIe9(ua) {
-  ua = ua || navigator.userAgent
-  return ua.indexOf('MSIE 9') !== -1
+function isIe9() {
+  return false;
 }
 
-function isHTTPS(protocol) {
-  protocol = protocol || 'https:'
-  return protocol === 'https:'
+function isHTTPS() {
+  return true;
 }
 
-function isIos(ua) {
-  ua = ua || global.navigator.userAgent
-  return /iPhone|iPod|iPad/i.test(ua)
+function isIos() {
+  return false;
 }
 
-function isAndroid(ua) {
-  ua = ua || global.navigator.userAgent
-  return /Android/.test(ua)
+function isAndroid() {
+  return false;
 }
 
-function isUnsupportedIosChrome(ua) {
-  var match, version
-
-  ua = ua || global.navigator.userAgent
-  match = ua.match(/CriOS\/(\d+)\./)
-
-  if (!match) {
-    return false
-  }
-
-  version = parseInt(match[1], 10)
-
-  return version < MINIMUM_SUPPORTED_CHROME_IOS_VERSION
+function isUnsupportedIosChrome() {
+  return false;
 }
 
-function supportsPopups(ua) {
-  ua = ua || global.navigator.userAgent
-  return !(
-    isIosWebview(ua) ||
-    isAndroidWebview(ua) ||
-    isOperaMini(ua) ||
-    isUnsupportedIosChrome(ua)
-  )
+function supportsPopups() {
+  return false;
 }
 
 // The Google Search iOS app is technically a webview and doesn't support popups.
-function isGoogleSearchApp(ua) {
-  return /\bGSA\b/.test(ua)
+function isGoogleSearchApp() {
+  return false;
 }
 
-function isIosWebview(ua) {
-  ua = ua || global.navigator.userAgent
-  if (isIos(ua)) {
-    if (isGoogleSearchApp(ua)) {
-      return true
-    }
-    return /.+AppleWebKit(?!.*Safari)/.test(ua)
-  }
-  return false
+function isIosWebview() {
+  return false;
 }
 
-function isAndroidWebview(ua) {
-  var androidWebviewRegExp = /Version\/[\d\.]+/
-
-  ua = ua || global.navigator.userAgent
-  if (isAndroid(ua)) {
-    return androidWebviewRegExp.test(ua) && !isOperaMini(ua)
-  }
-  return false
+function isAndroidWebview() {
+  return false;
 }
 
 module.exports = {

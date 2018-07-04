911024d51fc2fb8fcd914b10a5d382b08d83bd48
Peerapong Maitriwong <peerapong@code-app.com>
2018-07-04 19:13:13 +0700

    fixed ua.indexOf not found

--- src/client/browser-detection.js
+++ src/client/browser-detection.js
@@ -4,6 +4,6 @@ var isIe = require('@braintree/browser-detection/is-ie');
 var isIe9 = require('@braintree/browser-detection/is-ie9');
 
 module.exports = {
-  isIe: isIe,
-  isIe9: isIe9
+  isIe: () => true,
+  isIe9: () => true
 };
